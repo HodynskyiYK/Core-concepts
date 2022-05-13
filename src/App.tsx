9eca99cf2acd5a0a6367b9a1fce4772d653bd90b
Yurii Hodynskyi <y.hodynskyi@eisgroup.com>
2022-05-13 10:04:50 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react'
+
+const App: FC = () => {
+
+    return (
+        <div className="container">
+            <h1>Hello, React</h1>
+        </div>
+    )
+}
+
+export default App
