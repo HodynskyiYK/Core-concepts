1d115cb91acd50902495f817d083e034a8d23076
Yurii Hodynskyi <y.hodynskyi@eisgroup.com>
2022-05-13 15:32:37 +0300

    prepared project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React, {FC} from 'react'
 
+const mainTitle = 'Hello World'
+
 const App: FC = () => {
 
     return (
-        <div className="container">
-            <h1>Hello, React</h1>
-        </div>
+        <main className="pt-2">
+            <div className="container">
+                <h1>Hello World</h1>
+            </div>
+        </main>
     )
 }
 
